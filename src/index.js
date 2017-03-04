273cec57e9733dabca3c5d442ffa2e61acb4f00c
duanguang <492466509@qq.com>
2017-03-05 03:39:35 +0800

    调整输出对象方式

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /**
  * Created by duanguang on 2017/3/2.
  */
-//import * as dateFormat from './dateFormat';
-export  {timeagoFormat,formatDate,parseDate,formatDateToFriendly,formatDurationToFriendly,formatTimeToFriendly,getMonthDays,countDays} from './dateFormat';
+//export  {timeagoFormat,formatDate,parseDate,formatDateToFriendly,formatDurationToFriendly,formatTimeToFriendly,getMonthDays,countDays} from './dateFormat';
+
+export DateFormat from './dateFormat';
--- test/fun-test.js
+++ test/fun-test.js
@@ -1,45 +1,45 @@
 /**
  * Created by duanguang on 2017/3/2.
  */
-import {timeagoFormat,formatDate,parseDate,formatDateToFriendly,formatDurationToFriendly,formatTimeToFriendly,getMonthDays,countDays} from '../lib/index';
-
+/*import {timeagoFormat,formatDate,parseDate,formatDateToFriendly,formatDurationToFriendly,formatTimeToFriendly,getMonthDays,countDays} from '../lib/index';*/
+import {DateFormat} from '../lib/index';
 describe('test1', () => {
     it('timeagoFormat', () => {
-       var s=timeagoFormat('2016-06-10 12:12:00','2016-06-10 12:14:12');
+       var s=DateFormat.timeagoFormat('2016-06-10 12:12:00','2016-06-10 12:14:12');
         console.log(s);
     });
 
     it('formatDate', () => {
-       /* console.log(formatDate());
-        console.log(formatDate(new Date(), 'yyyy-MM-dd'));
-        console.log(formatDate(new Date(),'yyyy-MM-dd 第q季度 ww HH:mm:ss:SSS'));
-        console.log(formatDate(1472793615764));*/
+        console.log(DateFormat.formatDate());
+        console.log(DateFormat.formatDate(new Date(), 'yyyy-MM-dd'));
+        console.log(DateFormat.formatDate(new Date(),'yyyy-MM-dd 第q季度 ww HH:mm:ss:SSS'));
+        console.log(DateFormat.formatDate(1472793615764));
     });
 
     it('parseDate', () => {
-        /*console.log(parseDate('2016-08-11'));
-        console.log(parseDate('2016-08-11 13:28:43', 'yyyy-MM-dd HH:mm:ss'));*/
+        console.log(DateFormat.parseDate('2016-08-11'));
+        console.log(DateFormat.parseDate('2016-08-11 13:28:43', 'yyyy-MM-dd HH:mm:ss'));
     });
 
     it('formatDateToFriendly', () => {
-        console.log(formatDateToFriendly(new Date()));
+        console.log(DateFormat.formatDateToFriendly(new Date()));
     });
 
     it('formatDurationToFriendly', () => {
-        console.log(formatDurationToFriendly(168));
+        console.log(DateFormat.formatDurationToFriendly(168));
     });
 
     it('formatTimeToFriendly', () => {
-        console.log(formatTimeToFriendly(168));
+        console.log(DateFormat.formatTimeToFriendly(168));
     });
 
     it('getMonthDays', () => {
-        console.log(getMonthDays());
-        console.log(getMonthDays(new Date()));
-        console.log(getMonthDays(2013, 2));
+        console.log(DateFormat.getMonthDays());
+        console.log(DateFormat.getMonthDays(new Date()));
+        console.log(DateFormat.getMonthDays(2013, 2));
     });
 
     it('countDays', () => {
-        console.log(countDays('2017-03-02','2017-01-28'));
+        console.log(DateFormat.countDays('2017-03-02','2017-01-28'));
     });
 });
